4a3ab84a5ef8ebbad5bb11cca19474f0284989a9
Mostafa Osman <mostafa233.osman@gmail.com>
2025-04-24 22:25:13 +0200

    change padding

--- lib/views/widgets/notes_view_item.dart
+++ lib/views/widgets/notes_view_item.dart
@@ -6,15 +6,19 @@ class NotesListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4),
-          child: const NoteItem(),
-        );
-
-      },
-
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        padding:  EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: const NoteItem(),
+          );
+      
+        },
+      
+      ),
     );
   }
 }
