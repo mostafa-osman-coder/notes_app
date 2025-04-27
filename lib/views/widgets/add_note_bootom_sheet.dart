02706906a9e3483ec8995b51749f56699711631f
Mostafa Osman <mostafa233.osman@gmail.com>
2025-04-27 23:08:01 +0300

    finsh notes bottom sheet

--- lib/views/widgets/add_note_bootom_sheet.dart
+++ lib/views/widgets/add_note_bootom_sheet.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
+import 'custom_botton.dart';
+
 class AddNoteBotttomSheet extends StatelessWidget {
   const AddNoteBotttomSheet({super.key});
 
@@ -8,25 +10,36 @@ class AddNoteBotttomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: const [
-          const SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: 'title',
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomTextField(hint: 'content',
-          maxLines: 4,)
-
-
-          
-
-        ],
+      child: SingleChildScrollView(
+        child:  Column(
+          children: const [
+             SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: 'title',
+            ),
+             SizedBox(
+              height: 32,
+            ),
+            CustomTextField(hint: 'content',
+            maxLines: 4,),
+            SizedBox(
+              height: 32,
+            ),
+            CustomBotton(),
+            SizedBox(
+              height: 16,
+            ),
+        
+        
+            
+        
+          ],
+        ),
       ),
     );
   }
 }
+
+
